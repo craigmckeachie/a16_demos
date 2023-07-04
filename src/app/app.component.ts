d3d81c670bda8c077b72cf9a492b08698b300651
Craig McKeachie <craigdmckeachie@gmail.com>
2023-07-04 17:46:44 -0400

    rxjs-subject-observable-unicast

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  template: ` Ready `,
+  template: ``,
   styles: [],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    const observable = new Observable((subscriber) => {
+      subscriber.next(Math.random());
+    });
+
+    // subscription 1
+    observable.subscribe((data) => {
+      console.log(data); // 0.24957144215097515 (random number)
+    });
+
+    // subscription 2
+    observable.subscribe((data) => {
+      console.log(data); // 0.004617340049055896 (random number)
+    });
+  }
+}
