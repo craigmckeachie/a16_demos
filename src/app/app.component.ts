41de79a19d55207f3689b73f3c9fca36ed2548db
Craig McKeachie <craigdmckeachie@gmail.com>
2023-07-04 17:45:37 -0400

    event-binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: ` Ready `,
-  styles: [],
+  template: `
+    <a href="" (click)="onClick($event)">Click Me!</a>
+    <p [innerText]="message"></p>
+  `,
+  styles: []
 })
-export class AppComponent {}
+export class AppComponent {
+  message = '';
+
+  onClick(event) {
+    event.preventDefault();
+    this.message = 'clicked';
+  }
+}
