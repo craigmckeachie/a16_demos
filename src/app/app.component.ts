2a46359331d71ffa3e58a5ba7f64e175f3b1c549
Craig McKeachie <craigdmckeachie@gmail.com>
2023-07-04 17:46:06 -0400

    interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: ` Ready `,
-  styles: [],
+  template: `
+  <h2>{{image.name}}</h2>
+  <p>{{image.path}} </p>
+`,
+  styles: []
 })
-export class AppComponent {}
+export class AppComponent {
+  image = {
+    path: '../assets/angular_solidBlack.png',
+    name: 'Angular Logo'
+  };
+}
