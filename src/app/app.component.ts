03137661714183720aef3b54c03e5bd753d39d75
Craig McKeachie <craigdmckeachie@gmail.com>
2023-07-04 17:46:48 -0400

    template-forms-validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,32 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: ` Ready `,
-  styles: [],
+  template: `
+    <form #signinForm="ngForm" (submit)="onSubmit(signinForm)">
+      <input
+        type="text"
+        placeholder="username"
+        ngModel
+        #username="ngModel"
+        name="username"
+        required
+        minlength="3"
+      /><br />
+      <pre>
+        {{ username.errors | json }}
+      </pre
+      >
+      <br />
+      Dirty: {{ username.dirty | json }} <br />
+      Touched: {{ username.touched | json }} <br />
+      <input type="text" ngModel name="password" placeholder="password" /><br />
+      <button>Sign In</button>
+    </form>
+  `,
+  styles: []
 })
-export class AppComponent {}
+export class AppComponent {
+  onSubmit(form) {
+    console.log(form.value);
+  }
+}
