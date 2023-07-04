b8527cbf1e530372b4e28532bdb34674e8d75c0c
Craig McKeachie <craigdmckeachie@gmail.com>
2023-07-04 17:46:40 -0400

    rxjs-operators-map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,26 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { interval, Observer } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
-  template: ` Ready `,
+  template: `See console for output.`,
   styles: [],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    const observable$ = interval(1000);
+    const observer: Observer<number> = {
+      next: (x) => console.log(x),
+      complete: () => console.log('completed'),
+      error: (x) => console.log(x),
+    };
+
+    const observableCommingOutOfThePipe$ = observable$.pipe(
+      tap((x) => console.log(x)),
+      map((x) => x * 10)
+    );
+    const subscription = observableCommingOutOfThePipe$.subscribe(observer);
+  }
+}
