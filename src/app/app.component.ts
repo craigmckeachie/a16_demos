95a3245ed74abdb3ee4dc03e68536cf0dcfa5d45
Craig McKeachie <craigdmckeachie@gmail.com>
2023-07-04 17:46:45 -0400

    rxjs-subscription

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,21 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { interval, Observer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  template: ` Ready `,
+  template: `See console for output.`,
   styles: [],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    const observable$ = interval(1000);
+    const observer: Observer<number> = {
+      next: (x) => console.log(x),
+      complete: () => console.log('completed'),
+      error: (x) => console.log(x),
+    };
+    const subscription = observable$.subscribe(observer);
+    setTimeout(() => subscription.unsubscribe(), 5000);
+  }
+}
