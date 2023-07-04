2f703dcded2e583a9b8303e5e88357616d826d8e
Craig McKeachie <craigdmckeachie@gmail.com>
2023-07-04 17:46:07 -0400

    json-pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: ` Ready `,
-  styles: [],
+  template: `
+    <div>
+      <p>Without JSON pipe:</p>
+      <p>{{ object }}</p>
+      <p>With JSON pipe (no pre tag):</p>
+      <p>{{ object | json }}</p>
+      <p>With JSON pipe (and pre tag):</p>
+      <pre>{{ object | json }}</pre>
+    </div>
+  `,
+  styles: []
 })
-export class AppComponent {}
+export class AppComponent {
+  object: Object = {
+    foo: 'bar',
+    baz: 'qux',
+    nested: { xyz: 3, numbers: [1, 2, 3, 4, 5] }
+  };
+}
