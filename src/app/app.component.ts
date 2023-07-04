e5b837bfa9d0436ff3c8e15aaa01c90a982b5919
Craig McKeachie <craigdmckeachie@gmail.com>
2023-07-04 17:46:24 -0400

    reactive-forms-binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-root',
-  template: ` Ready `,
-  styles: [],
+  template: `
+  <h1>
+  Forms
+  </h1>
+  <form [formGroup]="loginForm" (submit)="onSubmit()">
+  <input formControlName="username" type="text" name="username">
+  <br>
+  <input formControlName="password" type="text" name="password">
+  <br>
+  <button>Sign In</button>
+  </form>
+  <pre>
+  {{loginForm.value | json}}
+  </pre>
+`,
+  styles: []
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  loginForm: FormGroup;
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl()
+    });
+  }
+  onSubmit() {
+    console.log(this.loginForm.value);
+  }
+}
