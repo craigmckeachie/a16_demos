2b73681558602e2bc997e06aeb82ecc24258ec81
Craig McKeachie <craigdmckeachie@gmail.com>
2023-07-04 17:46:41 -0400

    rxjs-operators-switchMap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,25 @@
+import { OnInit, ViewChild } from '@angular/core';
+import { ElementRef } from '@angular/core';
+import { AfterViewInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { fromEvent, interval } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
-  template: ` Ready `,
+  template: ` <button #myButton>Click Me</button> `,
   styles: [],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild('myButton') button: ElementRef | undefined = undefined;
+
+  ngOnInit(): void {}
+  ngAfterViewInit(): void {
+    fromEvent<Event>(this.button?.nativeElement, 'click')
+      .pipe(
+        //restart the counter on every click
+        switchMap(() => interval(1000))
+      )
+      .subscribe((x) => console.log(x));
+  }
+}
