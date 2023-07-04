ad158cd4acd4172a1b525dcb366dbec0b3190850
Craig McKeachie <craigdmckeachie@gmail.com>
2023-07-04 17:46:49 -0400

    template-forms-validation-messages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,33 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: ` Ready `,
-  styles: [],
+  template: `
+    <form #signinForm="ngForm" (submit)="onSubmit(signinForm)">
+      <input
+        type="text"
+        placeholder="username"
+        ngModel
+        #username="ngModel"
+        name="username"
+        required
+        minlength="3"
+      /><br />
+      <div *ngIf="username.hasError('required') && username.dirty">
+        Username is required.
+      </div>
+      <input
+        type="password"
+        ngModel
+        name="password"
+        placeholder="password"
+      /><br />
+      <button>Sign In</button>
+    </form>
+  `,
+  styles: []
 })
-export class AppComponent {}
+export class AppComponent {
+  onSubmit(form) {
+    console.log(form.value);
+  }
+}
