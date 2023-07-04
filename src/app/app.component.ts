8e302d4b4e05e189b472fca492b4569c08ccd224
Craig McKeachie <craigdmckeachie@gmail.com>
2023-07-04 17:46:21 -0400

    ngSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: ` Ready `,
-  styles: [],
+  template: `
+  <div [ngSwitch]="fruits.length">
+    <p *ngSwitchCase="0" >No records returned.</p>
+    <p *ngSwitchCase="1" >1 record returned.</p>
+    <p *ngSwitchDefault>{{fruits.length}} records were returned.</p>
+  </div>
+  <ul>
+    <li *ngFor="let fruit of fruits;
+               let i = index;">
+      {{i + 1}}.
+      {{fruit}}
+    </li>
+  </ul>
+  `,
+  styles: []
 })
-export class AppComponent {}
+export class AppComponent {
+  fruits = ['Apple', 'Orange', 'Plum'];
+}
