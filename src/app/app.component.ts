b588c94f3271524feae663bf5fc5344b44fb639a
Craig McKeachie <craigdmckeachie@gmail.com>
2023-07-04 17:45:23 -0400

    cdk-drag-drop-basic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: ` Ready `,
-  styles: [],
+  template: `
+    <div class="example-boundary">
+      <div class="example-box" cdkDragBoundary=".example-boundary" cdkDrag>
+        Drag me around
+      </div>
+    </div>
+  `,
+  styles: []
 })
 export class AppComponent {}
