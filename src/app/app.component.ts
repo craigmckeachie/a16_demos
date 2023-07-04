17977e7e92e11c4de72255807cb1f1e52a47fde4
Craig McKeachie <craigdmckeachie@gmail.com>
2023-07-04 17:46:36 -0400

    rxjs-observables-3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,22 @@
+import { OnInit, ViewChild } from '@angular/core';
+import { ElementRef } from '@angular/core';
+import { AfterViewInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  template: ` Ready `,
+  template: ` <input type="text" #myInput /> `,
   styles: [],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild('myInput') input: ElementRef | undefined = undefined;
+
+  ngOnInit(): void {}
+  ngAfterViewInit(): void {
+    const inputChangeEvents$ = fromEvent<InputEvent>(
+      this.input?.nativeElement,
+      'input'
+    ).subscribe((event: InputEvent) => console.log(event));
+  }
+}
