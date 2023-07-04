e7b06d7a3ae05ad9168ea7d85902234ce4059a86
Craig McKeachie <craigdmckeachie@gmail.com>
2023-07-04 17:46:28 -0400

    routing-basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: ` Ready `,
-  styles: [],
+  template: `
+    <nav>
+      <a routerLinkActive="active" [routerLink]="['/home']">Home</a> |
+      <a routerLinkActive="active" [routerLink]="['/about']">About</a> |
+      <a routerLinkActive="active" [routerLink]="['/contact']">Contact</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `,
+  styles: []
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'playground';
+}
