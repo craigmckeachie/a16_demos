ce84d3cca9f9c212d13f1ac339549cd6edf64bd1
Craig McKeachie <craigdmckeachie@gmail.com>
2023-07-04 17:45:30 -0400

    cdk-virtual-scrolling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,29 @@
 import { Component } from '@angular/core';
+import * as faker from 'faker';
 
 @Component({
   selector: 'app-root',
-  template: ` Ready `,
-  styles: [],
+  template: `
+    <cdk-virtual-scroll-viewport itemSize="50" class="list">
+      <div *cdkVirtualFor="let item of items" class="list-item">
+        {{ item }}
+      </div>
+    </cdk-virtual-scroll-viewport>
+  `,
+  styles: [
+    `
+      .list {
+        height: 200px;
+        width: 200px;
+        border: 1px solid black;
+      }
+
+      .list-item {
+        height: 50px;
+      }
+    `
+  ]
 })
-export class AppComponent {}
+export class AppComponent {
+  items = Array.from({ length: 100000 }).map(() => `${faker.name.findName()}`);
+}
