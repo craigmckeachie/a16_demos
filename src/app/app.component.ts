f5838d09e76cf7afa213039abf8f0d5fcbcc75e0
Craig McKeachie <craigdmckeachie@gmail.com>
2023-07-04 17:45:45 -0400

    http-error-handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,28 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { Photo } from './photo';
+import { PhotoService } from './photo.service';
 
 @Component({
   selector: 'app-root',
-  template: ` Ready `,
+  template: `
+    <h1>Photos</h1>
+    <p *ngIf="errorMessage">{{ errorMessage }}</p>
+    <div *ngFor="let photo of photos">
+      <img [src]="photo.thumbnailUrl" [alt]="photo.title" />
+      <p>{{ photo.title }}</p>
+    </div>
+  `,
   styles: [],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  photos: Photo[] = [];
+  errorMessage?: string | null;
+  constructor(private photoService: PhotoService) {}
+  ngOnInit(): void {
+    this.photoService.getAll().subscribe(
+      (data) => (this.photos = data),
+      (error) => (this.errorMessage = error)
+    );
+  }
+}
---
+++ src/app/photo.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { Photo } from './photo';
+import { catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PhotoService {
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<Photo[]> {
+    return this.http.get<Photo[]>('http://localhost:3000/photos/wrong').pipe(
+      catchError((error: HttpErrorResponse) => {
+        console.log(error);
+        return throwError('An error occured loading the photos.');
+      })
+    );
+  }
+}
