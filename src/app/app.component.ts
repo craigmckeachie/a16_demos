8f68256a522519b35e4a0ed5fa141eb79c4d8e9f
Craig McKeachie <craigdmckeachie@gmail.com>
2023-07-04 17:46:15 -0400

    ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: ` Ready `,
-  styles: [],
+  template: `
+    <div *ngIf="!isSignedIn">
+      <input type="text" placeholder="username" />
+      <input type="text" placeholder="password" />
+      <button (click)="signIn()">Sign In</button>
+    </div>
+    <div *ngIf="isSignedIn">Welcome back friend.</div>
+  `,
+  styles: []
 })
-export class AppComponent {}
+export class AppComponent {
+  isSignedIn = false;
+  signIn() {
+    this.isSignedIn = true;
+  }
+}
