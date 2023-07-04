4832f332870a54c122cda2c627469d27b9c2da89
Craig McKeachie <craigdmckeachie@gmail.com>
2023-07-04 17:46:37 -0400

    rxjs-observer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { OnInit } from "@angular/core";
+import { Component } from "@angular/core";
+import { of, Observer } from "rxjs";
 
 @Component({
-  selector: 'app-root',
-  template: ` Ready `,
+  selector: "app-root",
+  template: ` See console for output. `,
   styles: [],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    const observable$ = of(1, 2, 3);
+    const observer: Observer<number> = {
+      next: (x) => console.log(x),
+      complete: () => console.log("completed"),
+      error: (x) => console.log(x),
+    };
+    observable$.subscribe(observer);
+    observable$.subscribe(observer.next, observer.error, observer.complete);
+  }
+}
