767aff23806d58c1d2a622d29df85eda9e6d54b2
Craig McKeachie <craigdmckeachie@gmail.com>
2023-07-04 17:46:20 -0400

    ngStyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,39 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: ` Ready `,
-  styles: [],
+  template: `
+    <p (click)="onClick()"
+    [style.background-color]="'orchid'">
+    We need to button up ...
+    </p>
+    <p [ngStyle]="{'background-color': 'lime',
+    'font-size': '20px',
+    'font-weight': 'bold'}">
+    Here we go...
+    </p>
+  `,
+  styles: [
+    `
+      .highlight {
+        background-color: #ffff00;
+      }
+      .underline {
+        text-decoration: underline;
+      }
+    `
+  ]
 })
-export class AppComponent {}
+export class AppComponent {
+  isHighlighted = false;
+
+  onClick() {
+    this.isHighlighted = !this.isHighlighted;
+  }
+
+  calculateClasses() {
+    return {
+      highlight: this.isHighlighted,
+      underline: true
+    };
+  }
+}
