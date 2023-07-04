223dfe984aba945607726d417d42d9b69436ece9
Craig McKeachie <craigdmckeachie@gmail.com>
2023-07-04 17:46:33 -0400

    rxjs-behaviorsubject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,27 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  template: ` Ready `,
+  template: ` start `,
   styles: [],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    const subject = new BehaviorSubject(123);
+
+    // two new subscribers will get initial value => output: 123, 123
+    subject.subscribe(console.log);
+    subject.subscribe(console.log);
+
+    // two subscribers will get new value => output: 456, 456
+    subject.next(456);
+
+    // new subscriber will get latest value (456) => output: 456
+    subject.subscribe(console.log);
+
+    // all three subscribers will get new value => output: 789, 789, 789
+    subject.next(789);
+  }
+}
