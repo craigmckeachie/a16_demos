73d0da27d0cb4cab4c47b73ca1c3de7797b2811a
Craig McKeachie <craigdmckeachie@gmail.com>
2023-07-04 17:46:43 -0400

    rxjs-subject-multicast

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  template: ` Ready `,
+  template: ``,
   styles: [],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    const subject = new Subject();
+
+    // subscription 1
+    subject.subscribe((data) => {
+      console.log(data); // 0.24957144215097515 (random number)
+    });
+
+    // subscription 2
+    subject.subscribe((data) => {
+      console.log(data); // 0.24957144215097515(same random number)
+    });
+
+    subject.next(Math.random());
+  }
+}
