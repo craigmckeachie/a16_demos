de5aae5366a4d8fa6e2aca77675f98c2e57475f0
Craig McKeachie <craigdmckeachie@gmail.com>
2023-07-04 17:46:47 -0400

    template-forms-binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: ` Ready `,
-  styles: [],
+  template: `
+    <form #signinForm="ngForm" (submit)="onSubmit(signinForm)">
+      <input type="text" ngModel name="username" placeholder="username" /><br />
+      <input type="text" ngModel name="password" placeholder="password" /><br />
+      <button>Sign In</button>
+    </form>
+    <pre>
+      {{ signinForm.value | json }}
+    </pre
+    >
+  `,
+  styles: []
 })
-export class AppComponent {}
+export class AppComponent {
+  onSubmit(form) {
+    console.log(form.value);
+  }
+}
