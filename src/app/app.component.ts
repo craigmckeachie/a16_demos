2bbd256fd32922d5704b93f88f5a1f33625bb432
Craig McKeachie <craigdmckeachie@gmail.com>
2023-07-04 17:46:42 -0400

    rxjs-practical

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { of, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
-  template: ` Ready `,
+  template: `
+    <input
+      type="text"
+      #term
+      (input)="search(term.value)"
+      placeholder="search"
+    />
+    <br />
+    <p *ngFor="let message of messages">{{ message }}</p>
+  `,
   styles: [],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  messages: string[] = [];
+  private searchTermStream$ = new Subject<string>();
+
+  ngOnInit(): void {
+    this.searchTermStream$
+      .pipe
+      // debounceTime(1000),
+      // distinctUntilChanged(),
+      // switchMap((term: string) => {
+      //   return of(`new observable: ${term}`);
+      // })
+      ()
+      .subscribe((term) => this.messages.push(`http call for: ${term}`));
+  }
+
+  search(term: string) {
+    this.searchTermStream$.next(term);
+  }
+}
