8df5098f5c00fa231c910985e56b85670719ff0f
Craig McKeachie <craigdmckeachie@gmail.com>
2023-07-04 17:46:35 -0400

    rxjs-observables-2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,24 @@
+import { OnInit, ViewChild } from '@angular/core';
+import { ElementRef } from '@angular/core';
+import { AfterViewInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  template: ` Ready `,
+  template: ` <button #myButton>Click Me</button> `,
   styles: [],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild('myButton') button: ElementRef | undefined = undefined;
+
+  ngOnInit(): void {
+    console.log(this.button); //undefined
+  }
+  ngAfterViewInit(): void {
+    console.log(this.button); //ElementRef
+    console.log(this.button?.nativeElement); //button
+    const clickEvents$ = fromEvent<Event>(this.button?.nativeElement, 'click');
+    clickEvents$.subscribe((event: Event) => console.log(event));
+  }
+}
